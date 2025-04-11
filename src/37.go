f659910ba002e23ee40dc64a85ca9225a5407256
deborahmiranda335 <deborahmiranda335@gmail.com>
2025-04-11 12:37:19 +0000

    Add 37.go

---
+++ src/37.go
@@ -0,0 +1,10 @@
+public class Main extends React.createElement {
+    private static final int[][][] table = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    @React.createElement("div", {}, "Please enter a number to calculate the sum:"));
+}
+

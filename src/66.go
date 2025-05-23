ac7ce3ed95c9478fdb655f948614fa2048695770
deborahmiranda335 <deborahmiranda335@gmail.com>
2025-05-23 07:14:32 +0000

    Add 66.go

---
+++ src/66.go
@@ -0,0 +1,15 @@
+function isPrime(num) {
+  if (num <= 1) {
+    return false;
+  }
+  for (let i = 2; i * i <= num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function sum(a, b) {
+  return a + b;
+}
